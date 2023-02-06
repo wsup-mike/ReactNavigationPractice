725154866da5516c404c6b7e356ed0bb96f5d160
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-06 16:18:44 -0600

    Add new data to Bakeries.tsx

--- screens/Bakeries.tsx
+++ screens/Bakeries.tsx
@@ -1,9 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
+import BakeryCard from "../components/BakeryCard";
 
 const BakeriesScreen = () => {
     return (
         <View>
             <Text>Bakeries Screen</Text>
+            <ScrollView>
+                <BakeryCard name="Rowie's Bakery" />
+                <BakeryCard name="Mom's Bakery" />
+                <BakeryCard name="Panera Foods" />
+                <BakeryCard name="Dinkel's Bakery" />
+                <BakeryCard name="Lutz's Bakery" />
+                <BakeryCard name="Red Ribbon Bakery" />
+            </ScrollView>
         </View>
     )
 };
