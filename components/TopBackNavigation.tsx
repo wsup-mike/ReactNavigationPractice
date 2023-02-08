87643fcdc1bf3341975136c54cbf85d1142e557b
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-08 11:40:16 -0600

    New component 4 back button for Bakery.tsx. TopBac

---
+++ components/TopBackNavigation.tsx
@@ -0,0 +1,18 @@
+import { useNavigation } from '@react-navigation/native';
+import { TouchableHighlight } from 'react-native';
+import {} from 'react-native-gesture-handler';
+
+
+const TopBackNavigation = () => {
+    const navigation = useNavigation();
+
+    return (
+        <View>
+            <TouchableHighlight>
+
+            </TouchableHighlight>
+        </View>
+    )
+};
+
+export default TopBackNavigation;
